c47ffa10e0c505161f421c4bc28ef00f9a8a33a3
Asim Malik <asmalik@outlook.com>
2019-02-17 20:29:38 +0000

    Add nav icons

--- index.js
+++ index.js
@@ -1,16 +1,78 @@
 /** @format */
 
-import { Navigation } from "react-native-navigation";
+import {Navigation} from "react-native-navigation";
 import App from './App';
+import Home from "./src/Home";
+import Settings from "./src/Settings";
 
 Navigation.registerComponent(`am.playground.WelcomeScreen`, () => App);
+Navigation.registerComponent(`am.playground.Home`, () => Home);
+Navigation.registerComponent(`am.playground.Settings`, () => Settings);
+
+const rootComponent = {
+    name: "am.playground.WelcomeScreen"
+};
+
+const bottomTabs = {
+    children: [
+        {
+            stack: {
+                children: [
+                    {
+                        component: {
+                            name: 'am.playground.WelcomeScreen',
+                        }
+                    }
+                ],
+                options: {
+                    bottomTab: {
+                        text: 'Welcome',
+                        icon: require('./images/one.png'),
+                        iconColor: '#1B4C77',
+                        selectedIconColor: '#EC008C',
+                        selectedTextColor: '#EC008C',
+                        selectedIcon: require('./images/one_selected.png')
+                    }
+                }
+            }
+        },
+       {
+            component: {
+                name: 'am.playground.Home',
+                options: {
+                    bottomTab: {
+                        text: 'Home',
+                        icon: require('./images/two.png'),
+                        iconColor: '#1B4C77',
+                        selectedIconColor: '#EC008C',
+                        selectedTextColor: '#EC008C'
+                    }
+                }
+            }
+        },
+        {
+            component: {
+                name: 'am.playground.Settings',
+                options: {
+                    bottomTab: {
+                        text: 'Settings',
+                        icon: require('./images/three.png'),
+                        iconColor: '#1B4C77',
+                        selectedIconColor: '#EC008C',
+                        selectedTextColor: '#EC008C'
+                    }
+                }
+            }
+        }
+    ],
+    options: {}
+};
+
 
 Navigation.events().registerAppLaunchedListener(() => {
     Navigation.setRoot({
-    root: {
-        component: {
-        name: "am.playground.WelcomeScreen"
+        root: {
+            bottomTabs
         }
-       }
     });
 });
--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {StyleSheet, View, Text} from 'react-native';
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        backgroundColor: '#F5FCFF'
     }
 })
 
